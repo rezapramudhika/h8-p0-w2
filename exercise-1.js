f173fd4812e5fd9d46ef03312c4be4b9754a6bad
Reza Pramudhika <rezapramudhika@gmail.com>
2017-12-18 15:10:25 +0700

    exercise 1

--- exercise-1.js
+++ exercise-1.js
@@ -1,24 +1,3 @@
-/* Terdapat beberapa data dibawah ini yaitu:
-33
-6
-'foo'
-'bar'
-true
-false
-
-Dan juga terdapat beberapa nama variabel yang boleh kamu buat antara lain:
-number1
-number2
-string1
-string2
-boolean1
-boolean2
-
-tugas kamu adalah melakukan assignment pada variabel-variabel yang tersedia 
-dengan data yang ada dan menuliskannya menggunakan console.log(). */
-
-/* Answer */
-
 var number1 = 33;
 var number2 = 6;
 var string1 = 'foo';
@@ -26,4 +5,9 @@ var string2 = 'bar';
 var boolean1 = true;
 var boolean2 = false;
 
-console.log(number1+ '\n' +number2+ '\n"' +string1+ '"\n"' +string2+ '"\n' +boolean1+ '\n' +boolean2);
+console.log(number1);
+console.log(number2);
+console.log(string1);
+console.log(string2);
+console.log(boolean1);
+console.log(boolean2);
