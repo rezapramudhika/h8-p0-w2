4ce2154428364609ef9f5d0908c5ba2ef2dfb187
Reza Pramudhika <rezapramudhika@gmail.com>
2017-12-18 15:07:21 +0700

    exercise 2

---
+++ exercise-2.js
@@ -0,0 +1,15 @@
+
+var nama = 'Mikael';
+var peran = '';
+
+console.log('Selamat datang di Dunia Proxytia, '+nama);
+
+if (peran === 'Ksatria'){
+    console.log('Halo Ksatria '+nama+', kamu dapat menyerang dengan senjatamu!');
+}else if(peran === 'Tabib'){
+    console.log('Halo Tabib '+nama+', kamu akan membantu temanmu yang terluka.');
+}else if(peran === 'Penyihir'){
+    console.log('Halo Penyihir '+nama+', ciptakan keajaiban yang membantu kemenanganmu!');
+}else if(peran === ''){
+    console.log('Halo '+nama+', Pilih peranmu untuk memulai game!');
+}
