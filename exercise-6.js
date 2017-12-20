ea99fbf89ccaadbf4add53da5b2f55426fffbfd2
Reza Pramudhika <rezapramudhika@gmail.com>
2017-12-20 07:41:31 +0700

    exercise 6 (revised)

--- exercise-6.js
+++ exercise-6.js
@@ -9,29 +9,29 @@ for (var i=1; i<=100; i++){
 }
 
 console.log('Pertambahan counter 2')
-for (var i=1; i<100; i++){
-    if (i=i+2){
+for (var i=1; i<100; i+=2){
+    if (i%3 == 0){
         console.log(i+' KELIPATAN 3');
     }else{
-        console.log('');
+        console.log(i+' bukan kelipatan 3');
     }
 }
 
 console.log('Pertambahan counter 5')
-for (var i=1; i<100; i++){
-    if (i=i+5){
+for (var i=1; i<100; i+=5){
+    if (i%6 == 0){
         if(i<=100)
         console.log(i+' KELIPATAN 6');
     }else{
-        console.log('');
+        console.log(i+' bukan kelipatan 6');
     }
 }
 
 console.log('Pertambahan counter 9')
-for (var i=1; i<100; i++){
-    if (i=i+9){
+for (var i=1; i<100; i+=9){
+    if (i%10 == 0){
         console.log(i+' KELIPATAN 10');
     }else{
-        console.log('');
+        console.log(i+' bukan kelipatan 10');
     }
 }
